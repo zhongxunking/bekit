427d789ce27de55850d6992950c7e2927f79dc5f
zhongxunking <zhongxunking@163.com>
2017-05-25 16:07:50 +0800

    EventBus代码优化：合并升序和降序缓存（无功能变更）

--- event/src/main/java/top/bekit/event/bus/EventBus.java
+++ event/src/main/java/top/bekit/event/bus/EventBus.java
@@ -18,10 +18,8 @@ import java.util.*;
 public class EventBus {
     // 监听器执行器
     private List<ListenerExecutor> listenerExecutors = new ArrayList<>();
-    // 优先级升序监听器执行器缓存
-    private Map<Class, List<ListenerExecutor>> priorityAscListenerExecutorsCache = new HashMap<>();
-    // 优先级降序监听器执行器缓存
-    private Map<Class, List<ListenerExecutor>> priorityDescListenerExecutorsCache = new HashMap<>();
+    // 监听器执行器缓存
+    private Map<Class, List<ListenerExecutor>> listenerExecutorsCache = new HashMap<>();
 
     /**
      * 注册监听器
@@ -42,17 +40,11 @@ public class EventBus {
      * @throws Throwable 执行过程中发生任何异常都会往外抛
      */
     public void dispatch(Object event) throws Throwable {
-        // 执行优先级升序
-        List<ListenerExecutor> cachedListenerExecutors = priorityAscListenerExecutorsCache.get(event.getClass());
-        if (cachedListenerExecutors != null) {
-            for (ListenerExecutor listenerExecutor : cachedListenerExecutors) {
-                listenerExecutor.execute(event);
-            }
-        }
-        // 执行优先级降序
-        cachedListenerExecutors = priorityDescListenerExecutorsCache.get(event.getClass());
-        if (cachedListenerExecutors != null) {
-            for (ListenerExecutor listenerExecutor : cachedListenerExecutors) {
+        // 获取该事件类型的监听器缓存
+        List<ListenerExecutor> theListenerExecutors = listenerExecutorsCache.get(event.getClass());
+        if (theListenerExecutors != null) {
+            // 执行监听器
+            for (ListenerExecutor listenerExecutor : theListenerExecutors) {
                 listenerExecutor.execute(event);
             }
         }
@@ -60,44 +52,32 @@ public class EventBus {
 
     // 刷新监听器缓存
     private void refreshListenerCache() {
-        // 刷新优先级升序缓存
-        priorityAscListenerExecutorsCache = new HashMap<>();
-        // 获取本总线所有的优先级升序事件类型
+        listenerExecutorsCache = new HashMap<>();
+        // 获取本总线所有的事件类型
         Set<Class> eventTypes = new HashSet<>();
         for (ListenerExecutor listenerExecutor : listenerExecutors) {
             eventTypes.addAll(listenerExecutor.getEventTypes(true));
-        }
-        // 根据事件类型设置缓存
-        for (Class eventType : eventTypes) {
-            // 获取指定事件类型的监听器
-            List<ListenerExecutor> cachedListenerExecutors = new ArrayList<>();
-            for (ListenerExecutor listenerExecutor : listenerExecutors) {
-                if (listenerExecutor.getEventTypes(true).contains(eventType)) {
-                    cachedListenerExecutors.add(listenerExecutor);
-                }
-            }
-            // 设置缓存
-            priorityAscListenerExecutorsCache.put(eventType, cachedListenerExecutors);
-        }
-
-        // 刷新优先级降序缓存
-        priorityDescListenerExecutorsCache = new HashMap<>();
-        // 获取本总线所有的优先级降序事件类型
-        eventTypes = new HashSet<>();
-        for (ListenerExecutor listenerExecutor : listenerExecutors) {
             eventTypes.addAll(listenerExecutor.getEventTypes(false));
         }
         // 根据事件类型设置缓存
         for (Class eventType : eventTypes) {
-            // 获取指定事件类型的监听器
-            List<ListenerExecutor> cachedListenerExecutors = new ArrayList<>();
+            // 特定事件类型的监听器缓存
+            List<ListenerExecutor> theListenerExecutors = new ArrayList<>();
+            // 获取指定事件类型的升序监听器
+            for (ListenerExecutor listenerExecutor : listenerExecutors) {
+                if (listenerExecutor.getEventTypes(true).contains(eventType)) {
+                    theListenerExecutors.add(listenerExecutor);
+                }
+            }
+            // 获取指定事件类型的降序监听器
             for (int i = listenerExecutors.size() - 1; i >= 0; i--) {
-                if (listenerExecutors.get(i).getEventTypes(false).contains(eventType)) {
-                    cachedListenerExecutors.add(listenerExecutors.get(i));
+                ListenerExecutor listenerExecutor = listenerExecutors.get(i);
+                if (listenerExecutor.getEventTypes(false).contains(eventType)) {
+                    theListenerExecutors.add(listenerExecutor);
                 }
             }
             // 设置缓存
-            priorityDescListenerExecutorsCache.put(eventType, cachedListenerExecutors);
+            listenerExecutorsCache.put(eventType, theListenerExecutors);
         }
     }
 }
