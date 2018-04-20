7126a2ac245b0aeec4d94764d26c4ecc8e01d7ec
zhongxunking <zhongxunking@163.com>
2018-04-20 12:15:02 +0800

    DefaultListenResolver更名为ClassListenResolver

--- event/src/main/java/org/bekit/event/annotation/listener/Listen.java
+++ event/src/main/java/org/bekit/event/annotation/listener/Listen.java
@@ -9,7 +9,6 @@
 package org.bekit.event.annotation.listener;
 
 import org.bekit.event.extension.ListenResolver;
-import org.bekit.event.extension.support.DefaultListenResolver;
 
 import java.lang.annotation.*;
 
@@ -24,7 +23,7 @@ public @interface Listen {
     /**
      * 监听解决器（默认DefaultListenResolver）
      */
-    Class<? extends ListenResolver> resolver() default DefaultListenResolver.class;
+    Class<? extends ListenResolver> resolver();
 
     /**
      * 是否按照优先级升序
