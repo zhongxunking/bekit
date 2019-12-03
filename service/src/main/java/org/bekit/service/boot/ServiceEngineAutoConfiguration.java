47fa06a57d43d069f703db225995cc6763e1bd7e
zhongxunking <zhongxunking@163.com>
2019-12-03 23:33:22 +0800

    service：简化

--- service/src/main/java/org/bekit/service/boot/ServiceEngineAutoConfiguration.java
+++ service/src/main/java/org/bekit/service/boot/ServiceEngineAutoConfiguration.java
@@ -8,10 +8,10 @@
  */
 package org.bekit.service.boot;
 
+import org.bekit.event.boot.EventBusAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.bekit.event.boot.EventBusAutoConfiguration;
 
 /**
  * 服务引擎自动配置类
@@ -20,6 +20,4 @@ import org.bekit.event.boot.EventBusAutoConfiguration;
 @AutoConfigureAfter(EventBusAutoConfiguration.class)
 @Import(ServiceEngineConfiguration.class)
 public class ServiceEngineAutoConfiguration {
-    // 服务引擎由ServiceEngineConfiguration进行配置
-    // 本配置类的作用就是在spring-boot项目中自动导入ServiceEngineConfiguration
 }
