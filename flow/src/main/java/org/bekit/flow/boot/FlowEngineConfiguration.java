980cea8821858c0e64c73d7acd9ef9330a4994b3
zhongxunking <zhongxunking@163.com>
2019-12-22 23:25:15 +0800

    flow：调整boot

--- flow/src/main/java/org/bekit/flow/boot/FlowEngineConfiguration.java
+++ flow/src/main/java/org/bekit/flow/boot/FlowEngineConfiguration.java
@@ -13,28 +13,29 @@ import org.bekit.flow.FlowEngine;
 import org.bekit.flow.engine.DefaultFlowEngine;
 import org.bekit.flow.flow.FlowsHolder;
 import org.bekit.flow.listener.DefaultFlowListener;
+import org.bekit.flow.locker.TheFlowLockersHolder;
+import org.bekit.flow.mapper.TheFlowMappersHolder;
 import org.bekit.flow.processor.ProcessorsHolder;
-import org.bekit.flow.transaction.FlowTxsHolder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 
 /**
- * 流程引擎配置类
- * （非spring-boot项目需手动引入本配置类完成流程引擎配置）
+ * 流程引擎配置
  */
 @Configuration
 @Import({EventBusConfiguration.class,
         FlowsHolder.class,
         ProcessorsHolder.class,
-        FlowTxsHolder.class,
+        TheFlowMappersHolder.class,
+        TheFlowLockersHolder.class,
         DefaultFlowListener.class})
 public class FlowEngineConfiguration {
     // 流程引擎
     @Bean
-    @DependsOn({"org.bekit.flow.flow.FlowsHolder", "org.bekit.flow.transaction.FlowTxsHolder"})     // 保证出现循环引用时不会出错
-    public FlowEngine flowEngine(FlowsHolder flowsHolder, FlowTxsHolder flowTxsHolder) {
-        return new DefaultFlowEngine(flowsHolder, flowTxsHolder);
+    @DependsOn("org.bekit.flow.flow.FlowsHolder")     // 保证出现循环引用时不会出错
+    public FlowEngine flowEngine(FlowsHolder flowsHolder) {
+        return new DefaultFlowEngine(flowsHolder);
     }
 }
