392293d585e58c05a16ef08aeabf450f87416581
zhongxunking <zhongxunking@163.com>
2019-12-22 23:19:59 +0800

    flow：简化FlowEngine

--- flow/src/main/java/org/bekit/flow/engine/DefaultFlowEngine.java
+++ flow/src/main/java/org/bekit/flow/engine/DefaultFlowEngine.java
@@ -8,13 +8,12 @@
  */
 package org.bekit.flow.engine;
 
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.bekit.flow.FlowEngine;
 import org.bekit.flow.flow.FlowExecutor;
 import org.bekit.flow.flow.FlowsHolder;
-import org.bekit.flow.transaction.FlowTxExecutor;
-import org.bekit.flow.transaction.FlowTxsHolder;
-import org.springframework.util.ClassUtils;
+import org.springframework.util.Assert;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,94 +21,31 @@ import java.util.Map;
 /**
  * 流程引擎默认实现类
  */
+@AllArgsConstructor
 public class DefaultFlowEngine implements FlowEngine {
     // 流程持有器
     private final FlowsHolder flowsHolder;
-    // 流程事务持有器
-    private final FlowTxsHolder flowTxsHolder;
 
-    public DefaultFlowEngine(FlowsHolder flowsHolder, FlowTxsHolder flowTxsHolder) {
-        this.flowsHolder = flowsHolder;
-        this.flowTxsHolder = flowTxsHolder;
+    @Override
+    public <T> T execute(String flow, T target) {
+        return execute(flow, target, null);
     }
 
     @Override
-    public <T> T start(String flow, T target) {
-        return start(flow, target, null);
-    }
-
-    @Override
-    public <T> T start(String flow, T target, Map<Object, Object> attachment) {
-        // 校验目标对象类型
-        checkClassOfTarget(target, flow);
-        // 构造目标上下文
-        TargetContext<T> targetContext = new TargetContext(target, reviseAttachment(attachment));
-        // 执行流程
-        executeFlow(flow, targetContext);
-
-        return targetContext.getTarget();
-    }
-
-    @Override
-    public <T> T insertTarget(String flow, T target, Map<Object, Object> attachment) {
-        // 校验目标对象类型
-        checkClassOfTarget(target, flow);
-        // 构造目标上下文
-        TargetContext<T> targetContext = new TargetContext(target, reviseAttachment(attachment));
-        // 执行插入目标对象
-        executeInsertTarget(flow, targetContext);
-
-        return targetContext.getTarget();
-    }
-
-    @Override
-    public <T> T insertTargetAndStart(String flow, T target, Map<Object, Object> attachment) {
-        // 校验目标对象类型
-        checkClassOfTarget(target, flow);
-        // 构造目标上下文
-        TargetContext<T> targetContext = new TargetContext(target, reviseAttachment(attachment));
-        // 执行插入目标对象
-        executeInsertTarget(flow, targetContext);
-        // 执行流程
-        executeFlow(flow, targetContext);
-
-        return targetContext.getTarget();
-    }
-
-    // 校验目标对象类型
-    private void checkClassOfTarget(Object target, String flow) {
+    public <T> T execute(String flow, T target, Map<Object, Object> attachment) {
+        Assert.notNull(target, "目标对象不能为null");
+        attachment = attachment != null ? attachment : new HashMap<>();
+        // 构建流程上下文
+        FlowContext<T> context = new FlowContext<>(target, attachment);
+        // 获取流程执行器
         FlowExecutor flowExecutor = flowsHolder.getRequiredFlowExecutor(flow);
-        if (!flowExecutor.getClassOfTarget().isAssignableFrom(target.getClass())) {
-            throw new IllegalArgumentException(String.format("传入的目标对象的类型[%s]和流程%s期望的类型[%s]不匹配", ClassUtils.getShortName(target.getClass()), flowExecutor.getFlowName(), ClassUtils.getShortName(flowExecutor.getClassOfTarget())));
-        }
-    }
-
-    // 修正附件
-    private Map<Object, Object> reviseAttachment(Map<Object, Object> attachment) {
-        return attachment != null ? attachment : new HashMap<>();
-    }
-
-    // 执行插入目标对象
-    private void executeInsertTarget(String flow, TargetContext targetContext) {
         try {
-            // 获取流程事务执行器
-            FlowTxExecutor flowTxExecutor = flowTxsHolder.getRequiredFlowTxExecutor(flow);
-            // 插入目标对象
-            flowTxExecutor.insertTarget(targetContext);
-        } catch (Throwable e) {
-            ExceptionUtils.rethrow(e);
-        }
-    }
-
-    // 执行流程
-    private void executeFlow(String flow, TargetContext targetContext) {
-        try {
-            // 获取流程执行器
-            FlowExecutor flowExecutor = flowsHolder.getRequiredFlowExecutor(flow);
             // 执行流程
-            flowExecutor.execute(targetContext);
+            flowExecutor.execute(context);
         } catch (Throwable e) {
             ExceptionUtils.rethrow(e);
         }
+
+        return context.getTarget();
     }
 }
