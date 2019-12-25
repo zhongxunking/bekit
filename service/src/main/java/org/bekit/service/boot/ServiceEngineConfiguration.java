d8ec74fb35e08f59b5b07cbe1527291154563bd1
zhongxunking <zhongxunking@163.com>
2019-12-26 00:00:30 +0800

    service：重构注册器

--- service/src/main/java/org/bekit/service/boot/ServiceEngineConfiguration.java
+++ service/src/main/java/org/bekit/service/boot/ServiceEngineConfiguration.java
@@ -8,30 +8,68 @@
  */
 package org.bekit.service.boot;
 
+import org.bekit.common.scanner.AbstractScanner;
+import org.bekit.common.transaction.TransactionManager;
 import org.bekit.event.EventPublisher;
 import org.bekit.event.boot.EventBusConfiguration;
 import org.bekit.event.bus.EventBusesHolder;
 import org.bekit.event.publisher.DefaultEventPublisher;
 import org.bekit.service.ServiceEngine;
+import org.bekit.service.annotation.service.Service;
 import org.bekit.service.engine.DefaultServiceEngine;
 import org.bekit.service.listener.ServiceListenerType;
-import org.bekit.service.service.ServicesHolder;
+import org.bekit.service.service.ServiceExecutor;
+import org.bekit.service.service.ServiceParser;
+import org.bekit.service.service.ServiceRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.util.Assert;
 
 /**
  * 服务引擎配置
  */
 @Configuration
-@Import({EventBusConfiguration.class, ServicesHolder.class})
+@Import({EventBusConfiguration.class,
+        ServiceRegistrar.class,
+        ServiceEngineConfiguration.ServiceScanner.class})
 public class ServiceEngineConfiguration {
-    // 服务引擎
+    /**
+     * 优先级
+     */
+    public static final int ORDER = 0;
+
+    // 配置服务引擎
     @Bean
-    @DependsOn({"org.bekit.service.service.ServicesHolder", "org.bekit.event.bus.EventBusesHolder"})    // 保证出现循环引用时不会出错
-    public ServiceEngine serviceEngine(ServicesHolder servicesHolder, EventBusesHolder eventBusesHolder) {
+    public ServiceEngine serviceEngine(ServiceRegistrar serviceRegistrar, EventBusesHolder eventBusesHolder) {
         EventPublisher eventPublisher = new DefaultEventPublisher(eventBusesHolder.getEventBus(ServiceListenerType.class));
-        return new DefaultServiceEngine(servicesHolder, eventPublisher);
+        return new DefaultServiceEngine(serviceRegistrar, eventPublisher);
+    }
+
+    /**
+     * 服务扫描器
+     */
+    @Order(ORDER)
+    public static class ServiceScanner extends AbstractScanner {
+        // 服务注册器
+        private final ServiceRegistrar serviceRegistrar;
+        // 事务管理器
+        private final TransactionManager transactionManager;
+
+        public ServiceScanner(ServiceRegistrar serviceRegistrar, TransactionManager transactionManager) {
+            super(Service.class);
+            this.serviceRegistrar = serviceRegistrar;
+            this.transactionManager = transactionManager;
+        }
+
+        @Override
+        protected void onScan(Object obj) {
+            // 解析
+            ServiceExecutor serviceExecutor = ServiceParser.parseService(obj, transactionManager);
+            // 注册
+            ServiceExecutor existedOne = serviceRegistrar.register(serviceExecutor);
+            Assert.isNull(existedOne, String.format("存在重名的服务[%s]", serviceExecutor.getServiceName()));
+        }
     }
 }
--- service/src/main/java/org/bekit/service/engine/DefaultServiceEngine.java
+++ service/src/main/java/org/bekit/service/engine/DefaultServiceEngine.java
@@ -15,7 +15,7 @@ import org.bekit.service.event.ServiceApplyEvent;
 import org.bekit.service.event.ServiceExceptionEvent;
 import org.bekit.service.event.ServiceFinishEvent;
 import org.bekit.service.service.ServiceExecutor;
-import org.bekit.service.service.ServicesHolder;
+import org.bekit.service.service.ServiceRegistrar;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.Assert;
 
@@ -27,8 +27,8 @@ import java.util.Map;
  */
 @AllArgsConstructor
 public class DefaultServiceEngine implements ServiceEngine {
-    // 服务持有器
-    private final ServicesHolder servicesHolder;
+    // 服务注册器
+    private final ServiceRegistrar serviceRegistrar;
     // 服务事件发布器
     private final EventPublisher eventPublisher;
 
@@ -39,28 +39,29 @@ public class DefaultServiceEngine implements ServiceEngine {
 
     @Override
     public <O, R> R execute(String service, O order, Map<Object, Object> attachment) {
+        // 获取服务执行器
+        ServiceExecutor serviceExecutor = serviceRegistrar.get(service);
+        Assert.notNull(serviceExecutor, String.format("服务[%s]不存在", service));
         // 校验order
-        checkOrder(order, service);
+        checkOrder(order, serviceExecutor);
         // 构建服务上下文
-        ServiceContext<O, R> context = new ServiceContext<>(order, (R) newResult(service), reviseAttachment(attachment));
+        ServiceContext<O, R> context = new ServiceContext<>(order, (R) newResult(serviceExecutor), reviseAttachment(attachment));
         // 执行服务
-        executeService(service, context);
+        executeService(serviceExecutor, context);
 
         return context.getResult();
     }
 
     // 校验入参order
-    private void checkOrder(Object order, String service) {
+    private void checkOrder(Object order, ServiceExecutor serviceExecutor) {
         Assert.notNull(order, "order不能为null");
-        ServiceExecutor serviceExecutor = servicesHolder.getRequiredServiceExecutor(service);
         if (!serviceExecutor.getOrderClass().isAssignableFrom(order.getClass())) {
             throw new IllegalArgumentException(String.format("入参order的类型[%s]和服务[%s]期望的类型不匹配", order.getClass(), serviceExecutor.getServiceName()));
         }
     }
 
     // 创建result
-    private Object newResult(String service) {
-        ServiceExecutor serviceExecutor = servicesHolder.getRequiredServiceExecutor(service);
+    private Object newResult(ServiceExecutor serviceExecutor) {
         return BeanUtils.instantiate(serviceExecutor.getResultClass());
     }
 
@@ -70,20 +71,18 @@ public class DefaultServiceEngine implements ServiceEngine {
     }
 
     // 执行服务
-    private void executeService(String service, ServiceContext context) {
-        // 获取服务执行器
-        ServiceExecutor serviceExecutor = servicesHolder.getRequiredServiceExecutor(service);
+    private void executeService(ServiceExecutor serviceExecutor, ServiceContext context) {
         try {
             // 发布服务申请事件
-            eventPublisher.publish(new ServiceApplyEvent(service, context));
+            eventPublisher.publish(new ServiceApplyEvent(serviceExecutor.getServiceName(), context));
             // 执行服务
             serviceExecutor.execute(context);
         } catch (Throwable e) {
             // 发布服务异常事件
-            eventPublisher.publish(new ServiceExceptionEvent(service, e, context));
+            eventPublisher.publish(new ServiceExceptionEvent(serviceExecutor.getServiceName(), e, context));
         } finally {
             // 发布服务结束事件
-            eventPublisher.publish(new ServiceFinishEvent(service, context));
+            eventPublisher.publish(new ServiceFinishEvent(serviceExecutor.getServiceName(), context));
         }
     }
 }
